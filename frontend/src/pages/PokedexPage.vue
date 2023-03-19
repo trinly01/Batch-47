fcf1625ebbd02f9edd12cb938af13a7bdc03ff2f
Trinmar Boado <timi.boado@gmail.com>
2023-03-19 17:07:10 +0800

    events, @click, @keyup

--- frontend/src/pages/PokedexPage.vue
+++ frontend/src/pages/PokedexPage.vue
@@ -6,8 +6,9 @@
     </q-toolbar-title>
     <!-- <q-btn flat round dense icon="apps" class="q-mr-xs" /> -->
     <!-- <q-btn flat round dense icon="more_vert" /> -->
-    <q-input placeholder="Search" dark dense standout v-model="textSearch" input-class="text-right" class="q-ml-md" />
+    <q-input @keyup.enter="triggerSearch" placeholder="Search" dark dense standout v-model="textSearch" input-class="text-right" class="q-ml-md" />
     <!-- <input type="text" v-model="textSearch" /> -->
+    <q-btn @click="triggerSearch" flat class="bg-purple-8 q-ml-md" round dense icon="search" />
   </q-toolbar>
   <div class="row justify-center">
     {{ filteredPokemons.length }}
@@ -16,7 +17,7 @@
   </div>
 
   <div class="row q-gutter-sm q-pa-md justify-center">
-    <q-card v-for="poke in filteredPokemons" :key="poke.id" class="my-card">
+    <q-card v-for="poke in state.searchedPokemons" :key="poke.id" class="my-card">
       <q-img :src="poke.data.sprites.other['official-artwork'].front_default">
         <div class="absolute-bottom">
           <div class="text-h6">{{ poke.name }}</div>
@@ -50,11 +51,12 @@ const textSearch = ref('')
 
 const host = 'https://pokeapi.co/api/v2/pokemon'
 
-const pokemons = ref([])
+// const pokemons = ref([])
 
 const state = reactive({
   pokeType: 'all',
-  pokemons
+  pokemons: [],
+  searchedPokemons: []
 })
 const types = [
   'all',
@@ -65,6 +67,10 @@ const types = [
   'bug'
 ]
 
+function triggerSearch () {
+  state.searchedPokemons = search()
+}
+
 const filteredPokemons = computed(() => {
   if (state.pokeType === 'all' && !textSearch.value) return state.pokemons
   if (state.pokeType === 'all') return state.pokemons.filter(p => p.name.includes(textSearch.value))
@@ -73,9 +79,19 @@ const filteredPokemons = computed(() => {
   })
 })
 
+function search () {
+  if (state.pokeType === 'all' && !textSearch.value) return state.pokemons
+  if (state.pokeType === 'all') return state.pokemons.filter(p => p.name.includes(textSearch.value))
+  return state.pokemons.filter(poke => {
+    return poke.data.types.map(t => t.type.name).includes(state.pokeType) && poke.name.includes(textSearch.value)
+  })
+}
+
 onMounted(async () => {
   // create a fetch request that will get pokemons from https://pokeapi.co/api/v2/pokemon
   state.pokemons = await getPokemons()
+
+  triggerSearch()
 })
 
 async function getPokemons () {
