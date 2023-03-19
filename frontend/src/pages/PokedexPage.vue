30cdb8eedc434cfa2b0aa566097c059b7338bb21
Trinmar Boado <timi.boado@gmail.com>
2023-03-19 14:35:11 +0800

    create PokedexPage and modify route

---
+++ frontend/src/pages/PokedexPage.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+const greeting = ref('Hello World!')
+</script>
+
+<template>
+  <h1 class="greeting">{{ greeting }}</h1>
+</template>
+
+<style>
+.greeting {
+  color: red;
+  font-weight: bold;
+}
+</style>
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -2,11 +2,15 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    component: () => import('pages/PokedexPage.vue')
   },
+  // {
+  //   path: '/',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [
+  //     { path: '', component: () => import('pages/IndexPage.vue') }
+  //   ]
+  // },
 
   // Always leave this as last one,
   // but you can also remove it
