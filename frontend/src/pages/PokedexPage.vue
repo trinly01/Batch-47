a2b435b08c9af2a1d72a41baaf5b4f7086489d50
Trinmar Boado <timi.boado@gmail.com>
2023-03-19 15:07:03 +0800

    card component

--- frontend/src/pages/PokedexPage.vue
+++ frontend/src/pages/PokedexPage.vue
@@ -6,14 +6,25 @@
     </q-toolbar-title>
     <!-- <q-btn flat round dense icon="apps" class="q-mr-xs" /> -->
     <!-- <q-btn flat round dense icon="more_vert" /> -->
-    <q-input dark dense standout v-model="textSearch" input-class="text-right" class="q-ml-md">
-      <template v-slot:append>
-        <q-icon v-if="text === ''" name="search" />
-        <q-icon v-else name="clear" class="cursor-pointer" @click="text = ''" />
-      </template>
-    </q-input>
+    <q-input placeholder="Search" dark dense standout v-model="textSearch" input-class="text-right" class="q-ml-md" />
+    <!-- <input type="text" v-model="textSearch" /> -->
   </q-toolbar>
-  {{ textSearch }}
+
+  <div>
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom">
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </div>
+      </q-img>
+
+      <q-card-actions>
+        <q-btn flat>Action 1</q-btn>
+        <q-btn flat>Action 2</q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
 </template>
 
 <script setup>
@@ -21,9 +32,9 @@ import { ref } from 'vue'
 const textSearch = ref('Hello Batch 47')
 </script>
 
-<style>
-.greeting {
-  color: red;
-  font-weight: bold;
+<style scoped>
+.my-card {
+  width: 100%;
+  max-width: 250px;
 }
 </style>
