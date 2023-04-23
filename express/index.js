dae35994375ff16fc7776eaa1dc9a5d3fcafde75
Trinmar Boado <timi.boado@gmail.com>
2023-04-23 14:18:39 +0800

    usong Body parser and Post method

--- express/index.js
+++ express/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const axios = require('axios')
+const bodyParser = require('body-parser');
 
 const app = express()
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 port = 3002
 
@@ -18,4 +22,14 @@ app.get('/sum', (req, res) => {
 app.get('/sum/:num1/:num2', (req, res) => {
     console.log('req.params', req.params)
     res.send(req.params)
+})
+
+app.post('/sum', (req, res) => {
+    console.log('req.body', req.body)
+    res.send(req.body)
+})
+
+app.get('/poke', async (req, res) => {
+    const { data } = await axios.get('https://pokeapi.co/api/v2/ability/?limit=20&offset=20')
+    res.send(data)
 })
